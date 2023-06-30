3f1a5bfb2594aed40f1cc648c3d6478c6c97d7b6
atjpta <63082814+atjpta@users.noreply.github.com>
2023-06-30 16:35:54 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+                <NuxtLink class="btn btn-outline btn-primary" to="about/gwefoweof">chuyển sang about</NuxtLink>
+
         <WaveSongWave1 />
     </div>
 </template>
