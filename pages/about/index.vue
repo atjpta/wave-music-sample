04ffb46e5fab7a42f4770d82ccc9a7708cae9783
atjpta <63082814+atjpta@users.noreply.github.com>
2023-06-30 16:35:27 +0700

    Update index.vue

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,1 +1,17 @@
 
+<template>
+    <div>
+        <NuxtLink class="btn btn-outline btn-primary" to="/">chuyển sang home</NuxtLink>
+
+        <div class="text-6xl">
+            id:
+            {{ id }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+const id = ref()
+const route = useRoute()
+id.value = route.params.id
+</script>
